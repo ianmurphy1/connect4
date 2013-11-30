962134b1673d4e626c5fc0a87f0d1145b8073d44
Ian Murphy <iano200@gmail.com>
2013-11-30 19:03:22 +0000

    Start of getMove on human player

--- src/connect4/HumanPlayer.java
+++ src/connect4/HumanPlayer.java
@@ -1,5 +1,11 @@
 package connect4;
 
+
+import edu.princeton.cs.introcs.StdIn;
+import edu.princeton.cs.introcs.StdOut;
+
+import java.util.InputMismatchException;
+
 public class HumanPlayer extends IPlayer {
 
     public HumanPlayer(LocationState playerState) {
@@ -8,7 +14,32 @@ public class HumanPlayer extends IPlayer {
 
     @Override
     public int getMove(Board board) {
-        return 0;
+        int col = getInt("Enter column number: ");
+
+        int x = checkCol(col, board);
+        if (x >= 0) return x;
+
+        return -1;
+    }
+
+    private int checkCol(int col, Board board) {
+        for (int i = board.getNoRows() - 1; i >= 0; i++) {
+            if (board.getLocationState(new Location(col, i)) == LocationState.EMPTY) return col;
+        }
+        return -1;
+    }
+
+    private int getInt(String prompt) throws InputMismatchException {
+        StdOut.print(prompt);
+        try {
+            int output = StdIn.readInt();
+            return output;
+        } catch (InputMismatchException e) {
+            StdIn.readLine();
+            StdOut.println("Not valid");
+            StdOut.println("Go again.");
+        }
+        return -1;
     }
 
 }
